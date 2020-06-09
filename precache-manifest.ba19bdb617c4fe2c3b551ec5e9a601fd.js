b2ac82f2e4f9fc9b727c5db0aa7de0203807bfc9
Ilya Sheremetov <ilya@sheremetov.com>
2020-06-09 23:04:17 +0200

    Updates

---
+++ precache-manifest.ba19bdb617c4fe2c3b551ec5e9a601fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96afa1b1f8688c5d6d69361130fc0979",
+    "url": "//illya1074.github.io/addnote/index.html"
+  },
+  {
+    "revision": "52f88c6822b55f83af75",
+    "url": "//illya1074.github.io/addnote/static/css/main.a96d4ab0.chunk.css"
+  },
+  {
+    "revision": "3102b65defab4e2f69ba",
+    "url": "//illya1074.github.io/addnote/static/js/2.7230cc81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//illya1074.github.io/addnote/static/js/2.7230cc81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f88c6822b55f83af75",
+    "url": "//illya1074.github.io/addnote/static/js/main.91ceed12.chunk.js"
+  },
+  {
+    "revision": "0d8f9b99a67a7d2382d3",
+    "url": "//illya1074.github.io/addnote/static/js/runtime-main.f8a7dcc8.js"
+  }
+]);
